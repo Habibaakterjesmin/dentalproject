85db7d4361b3b41f539480557aaee62aaaa4fbce
Habibaakterjesmin <140093216+Habibaakterjesmin@users.noreply.github.com>
2023-09-08 00:54:46 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $(".banner_carousel").owlCarousel({
+        items:1,
+        nav:true,
+        loop:true,
+    });
+  });
+
+  $(document).ready(function(){
+    $(".brand_carousel").owlCarousel({
+        items:5,
+        nav:true,
+        dots:true,
+        loop:true,
+    });
+  });
